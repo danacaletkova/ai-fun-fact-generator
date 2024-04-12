d942d030c08e5d784db35458e6188429bdd461fd
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-04-12 11:18:33 +0800

    Incorporated AI api

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
+function showFact(response) {
+  new Typewriter("#fact", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
 function getFact(event) {
   event.preventDefault();
 
-  new Typewriter("#fact", {
-    strings: "Apples float...",
-    autoStart: true,
-    delay: 35,
-    cursor: "",
-  });
+  let topic = document.querySelector("#topic-input");
+  let key = "5o168182b8atd0a481fedf7024b43479";
+  let context =
+    "You are a fun fact generator programmed to provide fascinating and entertaining information about a chosen topic. Your responses should consist only of the fact itself and any supporting details, and the total length of the response must not exceed 500 characters.";
+  let prompt = `Please provide fun facts about ${topic.value}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiUrl).then(showFact);
 }
 
 let topicFormElement = document.querySelector("#topic-form");
